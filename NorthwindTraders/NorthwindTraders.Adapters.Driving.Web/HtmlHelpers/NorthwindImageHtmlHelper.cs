73e82c7692dc74b4a0a4e74ed91a2a179e25429a
NickMazurenka <mikalai_mazurenka@epam.com>
2019-05-19 23:41:54 +0300

    Add tag and html helpers

---
+++ NorthwindTraders/NorthwindTraders.Adapters.Driving.Web/HtmlHelpers/NorthwindImageHtmlHelper.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace NorthwindTraders.Adapters.Driving.Web.HtmlHelpers
+{
+    public static class NorthwindImageHtmlHelper
+    {
+        public static IHtmlContent NorthwindImage(this IHtmlHelper htmlHelper, int imageId, string imageText)
+            => new HtmlString($"<img src=\"images/{imageId}\" alt=\"{imageText}\">");
+    }
+}
---
+++ NorthwindTraders/NorthwindTraders.Adapters.Driving.Web/TagHelpers/NorthwindImageLinkTagHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace NorthwindTraders.Adapters.Driving.Web.TagHelpers
+{
+    [HtmlTargetElement("a", Attributes = "northwind-id")]
+    public class NorthwindImageLinkTagHelper : TagHelper
+    {
+        [HtmlAttributeName("northwind-id")]
+        public string ImageId { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("href", $"images/{ImageId}");
+        }
+    }
+}
